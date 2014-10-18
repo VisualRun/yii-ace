8b5c9dd43e20bd3a206d4e8055b0810f864eb658
visualrun <wxw0813@gmail.com>
2014-10-18 10:03:32 +0800

    edit url

--- protected/config/main.php
+++ protected/config/main.php
@@ -45,16 +45,16 @@ return array(
 
 		// uncomment the following to enable URLs in path-format
 
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'showScriptName'=>false,
-			'urlSuffix'=>'.html',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
+		#'urlManager'=>array(
+		#	'urlFormat'=>'path',
+		#	'showScriptName'=>false,
+		#	'urlSuffix'=>'.html',
+		#	'rules'=>array(
+		#		'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+		#		'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+		#		'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+		#	),
+		#),
 
 
 		// database settings are configured in database.php
