08925d5b464b82e5d06caaf92c03388851dc2dfa
visualrun <wxw0813@gmail.com>
2014-10-28 06:59:37 +0800

    edit params

--- protected/config/params.php
+++ protected/config/params.php
@@ -6,4 +6,5 @@ return array(
 	'gender'=>array('男','女'),
 	'task_type'=>array(1=>'主要任务',2=>'次要任务'),
 	'task_important_type'=>array('普通','高','较高','急'),
+	'task_status'=>array(0=>'等待',1=>'激活',2=>'完成',3=>'暂停',4=>'取消',5=>'关闭'),
 );
