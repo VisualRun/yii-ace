b3554af7bd3deae730f8e6a0ce4ede5c30d7cb9f
wxw <wxw0813@gmail.com>
2014-11-01 08:43:42 +0800

    commit

--- themes/classic/views/actions/_form.php
+++ themes/classic/views/actions/_form.php
@@ -121,7 +121,7 @@
 			else {
 				//console.log("error uploading file", reason, detail);
 			}
-			$('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+ 
+			$('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+
 			 '<strong>File upload error</strong> '+msg+' </div>').prependTo('#alerts');
 		}
 
